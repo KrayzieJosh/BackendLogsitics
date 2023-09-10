1ac1db7de095b76956488c1db5772a2d325ea190
Joshua Daniel Jacobs <114062249+KrayzieJosh@users.noreply.github.com>
2023-09-10 17:59:19 +0200

    added Controller and serviceImpl Tests

---
+++ src/test/java/za/ac/cput/service/impl/GPSServiceImplTest.java
@@ -0,0 +1,59 @@
+package za.ac.cput.service.impl;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.cput.domain.GPS;
+import za.ac.cput.factory.GPSFactory;
+import za.ac.cput.service.GPSService;
+import za.ac.cput.util.Helper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class GPSServiceImplTest {
+
+    @Autowired
+    private GPSService service; // Inject the GPSService bean
+
+    private static GPS gps = GPSFactory.createGPS(Helper.generateID(), "456-789-123", "654-987-321");
+
+    @Test
+    void a_create() {
+        GPS created = service.create(gps);
+        System.out.println("Create: " + created);
+        assertNotNull(created);
+    }
+
+    @Test
+    void b_read() {
+        GPS read = service.read(gps.getTripId());
+        System.out.println("Read: " + read);
+        assertNotNull(read);
+    }
+
+    @Test
+    void c_update() {
+        GPS updated = new GPS.Builder().copy(gps).setStoreLocationId("789-789-456")
+                .build();
+        //assertNotNull(service.update(updated));
+        System.out.println("Updated: " + updated);
+    }
+
+    @Test
+    void e_delete() {
+        String id = gps.getTripId();
+        boolean success = service.delete(id);
+        assertTrue(success);
+        System.out.println("Success: " + success);
+    }
+
+    @Test
+    void d_getAll() {
+        System.out.println("Show all:");
+        System.out.println(service.getAll());
+    }
+}
---
+++ src/test/java/za/ac/cput/service/impl/TrackingServiceImplTest.java
@@ -0,0 +1,59 @@
+package za.ac.cput.service.impl;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.cput.domain.Tracking;
+import za.ac.cput.factory.TrackingFactory;
+import za.ac.cput.service.TrackingService;
+import za.ac.cput.util.Helper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest // Use Spring Boot's test support to enable Spring context
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class TrackingServiceImplTest {
+
+    @Autowired
+    private TrackingService service; // Inject the TrackingService bean
+
+    private static Tracking tracking = TrackingFactory.createTracker(Helper.generateID(), 1, 11);
+
+    @Test
+    void a_create() {
+        Tracking created = service.create(tracking);
+        System.out.println("Create: " + created);
+        assertNotNull(created);
+    }
+
+    @Test
+    void b_read() {
+        Tracking read = service.read(tracking.getTripId());
+        System.out.println("Read: " + read);
+        assertNotNull(read);
+    }
+
+    @Test
+    void c_update() {
+        Tracking updated = new Tracking.Builder().copy(tracking).setDetours(7)
+                .build();
+        //assertNotNull(service.update(updated));
+        System.out.println("Updated: " + updated);
+    }
+
+    @Test
+    void e_delete() {
+        String id = tracking.getTripId();
+        boolean success = service.delete(id);
+        assertTrue(success);
+        System.out.println("Success: " + success);
+    }
+
+    @Test
+    void d_getAll() {
+        System.out.println("Show all:");
+        System.out.println(service.getAll());
+    }
+}
